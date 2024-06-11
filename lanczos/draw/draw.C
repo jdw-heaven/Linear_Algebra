abbf309bcaa06f631da9d2e1bd1459995021453c
jdw-heaven <jdw.heaven@gmail.com>
2024-06-11 14:01:28 +0800

    new

---
+++ lanczos/draw/draw.C
@@ -0,0 +1,53 @@
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TFile.h>
+#include <iostream>
+#include <fstream>
+
+void DrawGraph() {
+    // 打开文件
+    std::ifstream file("/home/heaven/Desktop/doc/Linear_Algebra/lanczos/draw/data.txt");
+    double x, y;
+    TGraph *graph = new TGraph();
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return;
+    }
+
+    // 读取数据
+    while (file >> x >> y) {
+        graph->SetPoint(graph->GetN(), x, y);
+    }
+
+    // 关闭文件
+    file.close();
+
+    // 设置标记样式和大小
+    graph->SetMarkerStyle(20); // 设置标记样式，20 是一个常用的圆形标记
+    graph->SetMarkerSize(0.2); // 设置标记大小，值越大点越大
+
+    // 创建画布
+    TCanvas *c1 = new TCanvas("c1", "Graph Example", 800, 600);
+
+    // 绘制图形
+    graph->Draw("AP");
+
+    // 保存画布为 ROOT 文件
+    TFile *fileRoot = new TFile("/home/heaven/Desktop/doc/Linear_Algebra/lanczos/draw/graph.root", "RECREATE");
+    c1->Write();
+    fileRoot->Close();
+
+    // 显示画布
+    c1->Update();
+
+    // 释放内存
+    delete fileRoot;
+    delete graph;
+    delete c1;
+}
+
+int draw() {
+    DrawGraph();
+    return 0;
+}
--- lanczos/lanczos.c
+++ lanczos/lanczos.c
@@ -6,7 +6,7 @@ spin-1/2 XXY 反铁磁链基态能量求解
 int main(void)
 {
     FILE *data;
-    data = fopen("data.txt", "w");
+    data = fopen("draw/data.txt", "w");
     init_genrand(time(NULL));
 
     for(double Delta = -1.5; Delta <1.51; Delta += 0.01){
@@ -16,6 +16,6 @@ int main(void)
 
     // double Delta = -1.0;
     // printf("%lf\n", m_lanczos(Delta));
-
+    fclose(data);
     return 0;
 }
